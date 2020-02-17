5ebc660fd54e93344e936015324a8766ccc0abd6
Elynejs <matthias.gomes03@gmail.com>
2020-02-17 02:30:58 +0100

    .

--- index.js
+++ index.js
@@ -12,6 +12,19 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
+    const args = msg.content.slice(config.prefix.length).trim().split(/ /);
+    const command = args.shift().toLowerCase();
+
+    switch(command) {
+    case 'start':
+        // not exactly sure yet
+        break;
+    case 'help':
+        // list of commands
+        break;
+    default:
+        msg.channel.send('command was not recognized. refer yourself to the \'help\' command.');
+    }
 });
 
 client.login(token.token);
